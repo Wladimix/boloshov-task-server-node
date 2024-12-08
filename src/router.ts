36f0b9d327924522182c120f19c3205bbcfc6f84
Vladimir Maximov <m.a.x.i.m.o.v@yandex.ru>
2024-12-08 23:41:38 +0300

    README.md

--- src/router.ts
+++ src/router.ts
@@ -3,7 +3,7 @@ import UserController from "./controllers/UserController";
 import { Methods, route } from "./server";
 
 export default function router() {
-    route('/user/get', Methods.GET, UserController.getUser);
-    route('/user/add', Methods.POST, UserController.addUser);
-    route('/user/edit', Methods.PUT, UserController.editUser);
+    route('/user/get',  Methods.GET,  UserController.getUser);
+    route('/user/add',  Methods.POST, UserController.addUser);
+    route('/user/edit', Methods.PUT,  UserController.editUser);
 }
